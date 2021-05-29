8e7d3806730dbe06a8832a4128f767f2716390c0
vikramsu <vs2091995@gmail.com>
2021-05-30 02:24:43 +0530

    first commit

--- main.js
+++ main.js
@@ -18,8 +18,29 @@ function clock(){
     document.querySelector('.minute').style.transform=`rotate(${minute}deg)`;
     document.querySelector('.second').style.transform=`rotate(${second}deg)`;
 }
-// document.querySelector('.dateDisplay')=innerHTML= dayjs(new Date()).format("ddd, MMM YYYY")
-document.querySelector('.timing >p').innerHTML= dayjs(new Date()).format("ddd, MMM YYYY")
-document.querySelector('.mobile_header >h4').innerHTML= dayjs(new Date()).format("ddd, MMM YYYY")
 
+document.querySelector('.timing >p').innerHTML= dayjs(new Date()).format("ddd, MMM YYYY")
+//document.querySelector('.mobile_header >h4').innerHTML= dayjs(new Date()).format("ddd, MMM YYYY")
 setInterval(clock,inc);
+
+function changeTheme(){
+    const  checkBox = document.getElementById("checkbox-btn");
+    if(checkBox.checked) {
+   // document.getElementById('theme_name').textContent="Dark"
+    theme.setAttribute("data-theme", "light")
+     document.getElementById('theme_image').setAttribute("src","moon.png")
+    }
+    else{
+       // document.getElementById('theme_name').textContent="Light"
+        const theme=document.getElementById('theme')
+        theme.setAttribute("data-theme", "dark")
+        document.getElementById('theme_image').setAttribute("src","sunny.png")
+    }
+}
+navigator.getBattery().then((batteryCallback)=>{
+    document.getElementById("battery_lavel").style.width=""+(parseInt(batteryCallback.level*100))+"%";
+    document.getElementById("battery_lavel").style.height="100%";
+    document.getElementById("battery_lavel").style.background="#000000";
+    document.getElementById("battery_text").textContent=parseInt(batteryCallback.level*100)+"%";
+
+})
